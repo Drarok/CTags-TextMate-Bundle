06c0b5a258f9c7723dec5e74510903b255a029d4
Drarok Ithaqua <github@catchall.drarok.com>
2011-12-09 02:05:42 +0000

    Include the file and line as (invalid) attributes.

--- CTags.tmbundle/Support/views/tag.php
+++ CTags.tmbundle/Support/views/tag.php
@@ -1,4 +1,4 @@
-	<div class="tag" rel="<?php echo htmlentities($tag->file); ?>">
+	<div class="tag" file="<?php echo htmlentities($tag->file); ?>" line="<?php echo $tag->line; ?>">
 		<span class="name"><?php echo $tag->name; ?></span>
 		<span class="file"><?php echo $tag->file; ?></span>
 	</div>
