4e3cee6a4a257eae35e50bf1e19e459b803b2132
Drarok Ithaqua <github@catchall.drarok.com>
2011-12-09 01:51:58 +0000

    Implement a tag view.

---
+++ CTags.tmbundle/Support/views/tag.php
@@ -0,0 +1,4 @@
+	<div class="tag" rel="<?php echo htmlentities($tag->file); ?>">
+		<span class="name"><?php echo $tag->name; ?></span>
+		<span class="file"><?php echo $tag->file; ?></span>
+	</div>
