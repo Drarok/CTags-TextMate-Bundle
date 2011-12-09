ce298ebd9e7d0f4282505ed1115d603c88c84140
Drarok Ithaqua <github@catchall.drarok.com>
2011-12-09 01:51:00 +0000

    Add stylesheet and javascript files to the template output.

--- CTags.tmbundle/Support/views/template.php
+++ CTags.tmbundle/Support/views/template.php
@@ -1,8 +1,14 @@
+<?php
+
+$styleSheet = BUNDLE_SUPPORT . 'css' . DS . 'style_screen.css';
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title><?php echo isset($title) ? $title : 'CTags'; ?></title>
+	<link rel="stylesheet" type="text/css" href="file://<?php echo $styleSheet; ?>" />
 </head>
 <body>
 <?php
@@ -10,6 +16,15 @@
 echo isset($content)
 	? $content
 	: null;
+
+if (isset($scripts)) {
+	array_unshift($scripts, 'jquery');
+	$scriptSkel = '<script type="text/javascript" src="file://%s"></script>';
+	foreach ($scripts as $script) {
+		$script = BUNDLE_SUPPORT . 'js' . DS . $script . '.js';
+		echo sprintf($scriptSkel, $script), PHP_EOL;
+	}
+}
 
 ?>
 </body>
