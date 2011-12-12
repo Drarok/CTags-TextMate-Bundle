063c4b65e6a19fc4f41982ac2ea854b729249b59
Mat Gadd <mgadd@names.co.uk>
2011-12-12 16:15:29 +0000

    Remove test data, perform real search.

--- CTags.tmbundle/Support/find_tag.php
+++ CTags.tmbundle/Support/find_tag.php
@@ -2,18 +2,15 @@
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
+if (! isset($_SERVER['TM_CURRENT_WORD'])) {
+	throw new Exception('Failed to find TM_CURRENT_WORD variable.');
+}
+
 // TODO: Find some tags.
-$foundTags = array(
-	(object) array(
-		'name' => 'Show Line 6',
-		'file' => __FILE__,
-		'line' => 6,
-	),
-	(object) array(
-		'name' => 'Just Open File',
-		'file' => __FILE__,
-	),
-);
+$parser = new CTagParser(CT_PROJECT_ROOT . 'tmtags');
+$foundTags = $parser->findTag($_SERVER['TM_CURRENT_WORD']);
+var_dump($foundTags);
+die();
 
 // Instantiate the template first.
 $template = View::factory('template');
