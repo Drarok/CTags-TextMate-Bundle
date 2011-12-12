21f5d51b8f2408140ef80fc4b4af79612d5549cb
Mat Gadd <mgadd@names.co.uk>
2011-12-12 14:27:33 +0000

    Fix output when tag has no line info.

--- CTags.tmbundle/Support/views/tag.php
+++ CTags.tmbundle/Support/views/tag.php
@@ -1,4 +1,8 @@
-	<div class="tag" file="<?php echo htmlentities($tag->file); ?>" line="<?php echo $tag->line; ?>">
+	<?php echo HTML::tag('div', array(
+		'class'=> 'tag',
+		'file' => $tag->file,
+		'line' => isset($tag->line) ? $tag->line : null,
+		'title' => $tag->file,
+	)); ?>
 		<span class="name"><?php echo $tag->name; ?></span>
-		<span class="file"><?php echo $tag->file; ?></span>
 	</div>
