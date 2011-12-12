818fcd4c76116439b27333e6c35de952d6bbebfc
Mat Gadd <mgadd@names.co.uk>
2011-12-12 15:19:25 +0000

    Start work on the update_tags command.

--- CTags.tmbundle/Support/update_tags.php
+++ CTags.tmbundle/Support/update_tags.php
@@ -2,3 +2,31 @@
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
+if (! isset($_SERVER['CT_PROJECT_ROOT'])) {
+	throw new Exception(
+		'You must set the \'CT_PROJECT_ROOT\' '
+		. 'environment variable in your Project.');
+}
+
+// Shorthand for the project root.
+define('CT_PROJECT_ROOT', rtrim($_SERVER['CT_PROJECT_ROOT'], DS) . DS);
+
+$cmd = implode(' ', array(
+	escapeshellcmd(BUNDLE_SUPPORT . 'bin' . DS . 'ctags'),
+	'-f',
+	escapeshellarg(CT_PROJECT_ROOT . 'tmtags'),
+	'--fields=Kn',
+	'--excmd=pattern',
+	'-R',
+	'--regex-PHP=\'/abstract class ([^ ]*)/\\1/c/\'',
+	'--regex-PHP=\'/interface ([^ ]*)/\\1/c/\'',
+	'--regex-PHP=\'/(public |static |abstract |protected |private )+function ([^ (]*)/\\2/f/\'',
+	escapeshellarg(CT_PROJECT_ROOT),
+));
+
+var_dump($cmd);
+
+$output = array();
+$exitCode = null;
+exec($cmd, $output, $exitCode);
+$output = implode(PHP_EOL, $output);
