a54eb635caac781e8e731fe4a8a5568651a89168
Philipp Fehre <philipp@fehre.co.uk>
2015-10-19 15:51:08 +0100

    use TM_PROJECT_DIRECTORY as CT_PROJECT_ROOT
    - Use `TM_PROJECT_DIRECTORY` when `CT_PROJECT_ROOT` is not set
    - escape bundle folder name as shellarg to deal with spaces in folder
      names
    - Show the file name in the result list

--- CTags.tmbundle/Support/views/tag.php
+++ CTags.tmbundle/Support/views/tag.php
@@ -4,6 +4,8 @@
 		'line' => isset($tag->line) ? $tag->line : null,
 		'title' => $tag->file,
 	)); ?>
+		<span class="file"><?php echo $tag->file; ?></span>
+		<br />
 		<span class="name"><?php echo $tag->name; ?></span>
 <?php if (isset($tag->line)) { ?>
 		<br />
